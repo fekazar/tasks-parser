b8e270be9c4569dd3fd91a830bb8961f2f3c0d22
Fyodor Kazarin <fekazar@gmail.com>
2023-10-25 23:23:45 +0300

    HTML formatting for messages

--- src/main/kotlin/edu/parsing/service/SubprocessNotifyService.kt
+++ src/main/kotlin/edu/parsing/service/SubprocessNotifyService.kt
@@ -4,14 +4,18 @@ import edu.parsing.model.Task
 import java.util.concurrent.Executors
 
 // todo: make html formatting
-fun Task.toTextMessage() = "'New task: $this'"
+fun Task.toTextMessage() = """
+    <b>$header </b>
+    <b>Цена: </b> $price
+    <a href="https://freelance.habr.com/tasks/$id">На страницу заказа </a>
+""".trimIndent()
 
 class SubprocessNotifyService : NotificationService {
     private val sendThread = Executors.newSingleThreadExecutor()
 
     override fun notifyAbout(task: Task) {
         sendThread.submit {
-            ProcessBuilder("telegram-send", task.toTextMessage())
+            ProcessBuilder("telegram-send", "--format", "html", "${task.toTextMessage()}")
                 .directory(null)
                 .redirectInput(ProcessBuilder.Redirect.INHERIT)
                 .redirectOutput(ProcessBuilder.Redirect.INHERIT)
