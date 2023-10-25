752bb636baa6b8b0290e5d24cf14b697ee134b60
Fyodor Kazarin <fekazar@gmail.com>
2023-10-25 23:04:23 +0300

    Messages queue impl

--- src/main/kotlin/edu/parsing/service/SubprocessNotifyService.kt
+++ src/main/kotlin/edu/parsing/service/SubprocessNotifyService.kt
@@ -1,17 +1,23 @@
 package edu.parsing.service
 
 import edu.parsing.model.Task
+import java.util.concurrent.Executors
 
 // todo: make html formatting
 fun Task.toTextMessage() = "'New task: $this'"
 
 class SubprocessNotifyService : NotificationService {
-    // todo: add tasks to pool to send messages steadily
+    private val sendThread = Executors.newSingleThreadExecutor()
+
     override fun notifyAbout(task: Task) {
-        ProcessBuilder("telegram-send", task.toTextMessage())
-            .directory(null)
-            .redirectInput(ProcessBuilder.Redirect.INHERIT)
-            .redirectOutput(ProcessBuilder.Redirect.INHERIT)
-            .start()
+        sendThread.submit {
+            ProcessBuilder("telegram-send", task.toTextMessage())
+                .directory(null)
+                .redirectInput(ProcessBuilder.Redirect.INHERIT)
+                .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+                .start()
+
+            Thread.sleep(3000)
+        }
     }
 }
