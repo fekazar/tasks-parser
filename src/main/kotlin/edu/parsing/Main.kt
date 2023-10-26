a3eec61199180a526bb5507a6f813da35b688358
Fyodor Kazarin <fekazar@gmail.com>
2023-10-26 10:35:12 +0300

    Cli args

--- src/main/kotlin/edu/parsing/Main.kt
+++ src/main/kotlin/edu/parsing/Main.kt
@@ -6,9 +6,18 @@ import edu.parsing.service.SubprocessNotifyService
 import edu.parsing.service.TaskService
 
 // Started with chrono
-fun main() {
-    val taskRepository = CsvRepository("tasks.csv")
-    val queryRepository = FileQueryRepository("queries.txt")
+fun main(args: Array<String>) {
+    val cliArgs = try {
+        args.toList()
+            .chunked(2)
+            .associate { it[0] to it[1] }
+    } catch (_: Exception) {
+        println("Error parsing command args: ${args.joinToString(separator = " ")}")
+        return
+    }
+
+    val taskRepository = CsvRepository(cliArgs["--tasks"] ?: "tasks.csv")
+    val queryRepository = FileQueryRepository(cliArgs["--queries"] ?: "queries.txt")
     val notificationService = SubprocessNotifyService()
 
     val taskService = TaskService(taskRepository, queryRepository, notificationService)
